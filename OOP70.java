ddc57995e88e13ac327260d2e91c98f47636c53b
Janghanju <hanju1215@naver.com>
2017-05-29 20:28:18 +0900

    2017.5.25

--- OOP70.java
+++ OOP70.java
@@ -3,7 +3,7 @@ package BIT02;
 public class OOP70 {
 
 	public static void main(String[] args) {
-		//정보올림피아드 알고리즘		정보 찾
+		//정보올림피아드 알고리즘		정보 찾기
 		
 		int[] a = {0,3,1,4,5,2};
 		int[] b = new int[a.length];
